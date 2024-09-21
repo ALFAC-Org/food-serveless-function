e8f82c442ecb4317210cbf86f7f1281192447eec
Leonardo Fraga <lafraga91@gmail.com>
2024-09-21 10:18:26 -0300

    Fix GH Actions to include node_modules in zip file

--- valida_cpf_usuario/index.js
+++ valida_cpf_usuario/index.js
@@ -1,31 +1,27 @@
 const mysql = require('mysql2/promise');
 
 exports.handler = async (event) => {
-    // Loga o evento completo para depuração
-    console.log("Received event:", JSON.stringify(event, null, 2));
 
     // Extrai o CPF dos atributos do usuário
-    const cpf = event.request.userAttributes.cpf;
-
-    // Loga o CPF para validação
-    console.log("Received CPF:", cpf);
+    const cpf = event.request.userAttributes['custom:cpf'];
 
     if (!cpf) {
+        // TODO: Retornar um token sem CPF
         return {
             statusCode: 400,
             body: JSON.stringify({ message: "CPF is required" }),
         };
     }
 
-    // Lógica para validar o CPF no RDS
     const isValid = await validateCPFInRDS(cpf);
 
     if (isValid) {
         return {
             statusCode: 200,
-            body: JSON.stringify({ token: generateJWT(cpf) }),
+            body: JSON.stringify({ token: callCognito(cpf) }),
         };
     } else {
+        // TODO: Call cognito to insert CPF in User Pool
         return {
             statusCode: 404,
             body: JSON.stringify({ message: "CPF not found" }),
@@ -34,24 +30,29 @@ exports.handler = async (event) => {
 };
 
 async function validateCPFInRDS(cpf) {
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    });
+    let connection
+    try {
+        connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+    }
+    catch (error) {
+        console.error('Error Creating db-connection:', error);
+    }
 
     try {
         const [rows] = await connection.execute('SELECT 1 FROM users WHERE cpf = ?', [cpf]);
         return rows.length > 0;
     } catch (error) {
         console.error('Error validating CPF in RDS:', error);
-        return false;
     } finally {
         await connection.end();
     }
 }
 
-function generateJWT(cpf) {
+function callCognito(cpf) {
     // Implementar a lógica para gerar um JWT contendo o CPF
 }
