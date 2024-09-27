e2a88fde92bf4322c911c935c538a503531d5f61
Leonardo Fraga <lafraga91@gmail.com>
2024-09-26 23:06:19 -0300

    Refactor

--- valida_cpf_usuario/index.js
+++ valida_cpf_usuario/index.js
@@ -1,22 +1,18 @@
-const { whois } = require("./whois.js")
 const { generate, validate } = require("./jwt.js")
 const { validateCPFInRDS } = require("./database.js")
 
 exports.handler = async (event) => {
-  const authorization = event.headers && event.headers.auth
+  const authorization = event.headers && event.headers.Auth
   const deny = {
     isAuthorized: false,
     context: {}
   }
 
-  console.log("FRAGA - EVENT", event)
-  console.log("FRAGA - AUTHORIZATION", authorization)
   // SCENARIO 1: User is already authenticated
   // So let's keep the current token
-  if (authorization) {
+  if (authorization.startWith("Bearer")) {
     const token = authorization.split(" ")[1]
     const decoded = validate(token)
-    console.log("FRAGA - DECODED", decoded)
 
     if (decoded) {
       return {
@@ -31,18 +27,16 @@ exports.handler = async (event) => {
     }
   }
 
-  const cpf = event.headers && event.headers.cpf
-  const who = whois(cpf) // ANONYMOUS, AUTHENTICATED, INVALID
-  console.log("FRAGA - WHO ln-36", who)
+  const isValidCpf = authorization.length === 11
 
   // SCENARIO 2: Someone is trying to authenticate with invalid data
-  if (who === "INVALID") {
+  if (!isValidCpf) {
     return deny
   }
 
   // SCENARIO 3: Someone is trying to authenticate as ANONYMOUS
-  if (who === "ANONYMOUS") {
-    const jwt = generate({ who })
+  if (authorization === "ANONYMOUS") {
+    const jwt = generate({ who: "ANONYMOUS" })
 
     return {
       isAuthorized: true,
@@ -53,28 +47,23 @@ exports.handler = async (event) => {
   }
 
   // SCENARIO 4: Someone is trying to authenticate as AUTHENTICATED
-  if (who === "AUTHENTICATED") {
-    try {
-      const isValid = await validateCPFInRDS(cpf)
-      console.log("FRAGA - IS VALID", isValid)
 
-      if (isValid) {
-        const jwt = generate({ who })
+  try {
+    const isValid = await validateCPFInRDS(authorization)
 
-        return {
-          isAuthorized: true,
-          context: {
-            jwt: `Bearer ${jwt}`
-          }
+    if (isValid) {
+      const jwt = generate({ who: "AUTHENTICATED", cpf: authorization })
+
+      return {
+        isAuthorized: true,
+        context: {
+          jwt: `Bearer ${jwt}`
         }
-      } else {
-        return deny
       }
-    } catch (error) {
+    } else {
       return deny
     }
+  } catch (error) {
+    return deny
   }
-
-  // ANY OTHER SCENARIO SHOULD BE CONSIDERED AS UNAUTHORIZED
-  return deny
 }
