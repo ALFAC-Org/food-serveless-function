8024698379f93a83474740dca2ec9bd8a17cb990
carlohcs <carlohcs@gmail.com>
2024-09-28 01:20:59 -0300

    chore: adiciona logs a lambda

--- valida_cpf_usuario/index.js
+++ valida_cpf_usuario/index.js
@@ -1,6 +1,10 @@
 const { generate, validate } = require("./jwt.js")
 const { validateCPFInRDS } = require("./database.js")
 
+const log = (message) => {
+  console.log(`[valida_cpf_usuario] ${message}`)
+}
+
 const handler = async (event) => {
   const headers = event.headers
   const authorization = headers && headers.auth
@@ -9,13 +13,19 @@ const handler = async (event) => {
     context: {}
   }
 
+  log(`Received request with headers: ${JSON.stringify(headers)}`)
+
   // SCENARIO 1: User is already authenticated
   // So let's keep the current token
   if (authorization.startsWith("Bearer")) {
     const token = authorization.split(" ")[1]
     const decoded = validate(token)
 
+    log(`We have a Bearer token`)
+
     if (decoded) {
+      log(`Authorizing token`)
+
       return {
         isAuthorized: true,
         context: {
@@ -23,6 +33,8 @@ const handler = async (event) => {
         }
       }
     } else {
+      log(`The Bearer token is expired or invalid`)
+
       // TODO: refresh token?
       return deny
     }
@@ -30,7 +42,11 @@ const handler = async (event) => {
 
   // SCENARIO 2: Someone is trying to authenticate as ANONYMOUS
   if (authorization.toUpperCase() === "ANONYMOUS") {
+    log(`We have an ANONYMOUS user`)
+
     const jwt = generate({ who: "ANONYMOUS" })
+
+    log(`Generating a token for it`)
 
     return {
       isAuthorized: true,
@@ -44,15 +60,23 @@ const handler = async (event) => {
 
   // SCENARIO 3: Someone is trying to authenticate with invalid data
   if (!isValidCpf) {
+    log(`Invalid cpf`)
+
     return deny
   }
 
   // SCENARIO 4: Someone is trying to authenticate as AUTHENTICATED
   try {
+    log(`Let's get the client id for the CPF`)
+
     const getClientId = await validateCPFInRDS(authorization)
 
     if (getClientId) {
+      log(`We got the client id for the CPF`)
+
       const jwt = generate({ who: "AUTHENTICATED", id: getClientId })
+
+      log(`Generating a token for it`)
 
       return {
         isAuthorized: true,
@@ -65,6 +89,8 @@ const handler = async (event) => {
     }
   } catch (error) {
     return deny
+  } finally {
+    log(`Finishing the request`)
   }
 }
 
